88768bf384d99337f23ddf4192b32fde4b76dc90
Tyler Martin <tylertmartin2020@gmail.com>
2023-01-28 14:35:59 -0500

    1.7 HW

--- YouAreAwesome2/ContentView.swift
+++ YouAreAwesome2/ContentView.swift
@@ -8,50 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
-   @State private var messageString = "When the Genius Bar needs help, they call you!"
+    @State private var messageString = ""
     
     var body: some View {
+        
+        
+        
+        VStack {
             
+            Spacer()
             
-            VStack {
-                
-                Spacer()
-                
-                Text(messageString)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.red)
-                    .padding()
-                    .frame(height: 150)
-                    .frame(maxWidth: .infinity)
-                    //.border(.orange, width: 1)
-                    .padding()
-                
-                Spacer()
-                
-                
-                HStack{
-                    
-                    Button("Awesome") {
-                        //This is the action performed when pushed
-                        messageString = "You Are Awesome!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                    
-                    Spacer()
-                    
-                    Button("Great") {
-                        messageString = "You Are Great!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                }
-                //.border(.purple, width: 5)
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
                 .padding()
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+            //.border(.orange, width: 1)
+                .padding()
+            
+            Spacer()
+            
+            
+            
+            
+            HStack{
+                
+                Button("Awesome") {
+                    //This is the action performed when pushed
+                    messageString = "You Are Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Spacer()
+                
+                
+                
+                Button("Great") {
+                    messageString = "You Are Great!"
+                }
+                .buttonStyle(.borderedProminent)
             }
+            //.border(.purple, width: 5)
+            .padding()
         }
     }
+}
+
 
 
 struct ContentView_Previews: PreviewProvider {
