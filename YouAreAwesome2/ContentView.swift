67c7b84233dbbc83c15c89692b47de72fd4f44a0
Tyler Martin <tylertmartin2020@gmail.com>
2023-02-04 13:31:18 -0500

    1.15 HW DONE

--- YouAreAwesome2/ContentView.swift
+++ YouAreAwesome2/ContentView.swift
@@ -10,26 +10,15 @@ import SwiftUI
 struct ContentView: View {
     @State private var messageString = ""
     @State private var imageName = ""
-    @State private var imageNumber = 0
-    @State private var messageNumber = 0
+    @State private var lastMessageNumber = -1
+    @State private var lastImageNumber = -1
     var body: some View {
         
         
         
         VStack {
             
-            Image(imageName)
-                .resizable()
-                .scaledToFit()
-                .cornerRadius(30)
-                .shadow(radius: 30)
-                .padding()
-            
-            Spacer()
-            
-            
-            
-            
+        
             
             Text(messageString)
                 .font(.largeTitle)
@@ -42,6 +31,17 @@ struct ContentView: View {
                 .frame(maxWidth: .infinity)
                 .padding()
             
+            
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(30)
+                .shadow(radius: 30)
+                .padding()
+            
+        
+            
             Spacer()
             
             
@@ -53,13 +53,26 @@ struct ContentView: View {
                                 "You Make Me Smile!",
                                 "When the Genius Bar Needs Help, They Call You!"]
                 
-                messageString = messages[Int.random(in: 0...messages.count-1)]
                 
                 
                 
-                imageName = "image\(Int.random(in: 0...9))"
+                var messageNumber = Int.random(in: 0...messages.count-1)
+                while messageNumber == lastMessageNumber {
+                    messageNumber = Int.random(in: 0...messages.count-1)
+                }
+                messageString = messages[messageNumber]
+                lastMessageNumber = messageNumber
                 
-               
+                
+               // imageName = "image\(Int.random(in: 0...9))"
+                
+                var imageNumber = Int.random(in: 0...9)
+                while imageNumber == lastImageNumber {
+                    imageNumber = Int.random(in: 0...9)
+                }
+                
+                imageName = "image\(imageNumber)"
+                lastImageNumber = imageNumber
                 
             }
             .buttonStyle(.borderedProminent)
